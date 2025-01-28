ec401214c6dc5a9cd7ea412eaf4e3db76396803c
javiroger <javier.roger@devoteam.com>
2025-01-28 17:35:13 +0100

    fix fmt

--- services/bigquery/tables.tf
+++ services/bigquery/tables.tf
@@ -4,19 +4,19 @@ locals {
 
 resource "google_bigquery_table" "gcs_external_table" {
   for_each            = { for item in local.flattened_origins : "${item.origin}=>${item.path}=>${item.table}" => item }
-  dataset_id               = each.value.origin
-  deletion_protection      = true
-  project                  = "pj-data-des"
-  schema = file("schemas/A980.json")
-  table_id = each.value.table
+  dataset_id          = each.value.origin
+  deletion_protection = true
+  project             = "pj-data-des"
+  schema              = file("schemas/A980.json")
+  table_id            = each.value.table
   external_data_configuration {
-    autodetect                = false
-    compression               = "GZIP"
-    connection_id             = "283405604430.eu.biglake-gcs"
-    ignore_unknown_values     = false
-    max_bad_records           = 0
-    source_format             = "CSV"
-    source_uris               = ["gs://gcs-pj-data-des-data-extraction/${each.value.path}/${each.value.table}/${each.value.table}_chunk*.csv.gz"]
+    autodetect            = false
+    compression           = "GZIP"
+    connection_id         = "283405604430.eu.biglake-gcs"
+    ignore_unknown_values = false
+    max_bad_records       = 0
+    source_format         = "CSV"
+    source_uris           = ["gs://gcs-pj-data-des-data-extraction/${each.value.path}/${each.value.table}/${each.value.table}_chunk*.csv.gz"]
     csv_options {
       allow_jagged_rows     = false
       allow_quoted_newlines = false
